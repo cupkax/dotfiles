545ab49f5000d97312970bb9e7458c29fe9b552d
Vedant <vedantsansare23@gmail.com>
2020-12-25 08:58:15 +0530

    doom emacs config

---
+++ editor/emacs/doom/config.el
@@ -0,0 +1,34 @@
+;;; config.el -*- lexical-binding: t; -*-
+
+(setq user-full-name "Vedant Sansare"
+      user-mail-address "vedantsansare23@.com")
+
+(setq-default
+ delete-by-moving-to-trash t                      ; Delete files to trash
+ window-combination-resize t                      ; take new window space from all other windows (not just current)
+ x-stretch-cursor t)                              ; Stretch cursor to the glyph width
+
+(setq undo-limit 80000000                         ; Raise undo-limit to 80Mb
+      evil-want-fine-undo t                       ; By default while in insert all changes are one big blob. Be more granular
+      auto-save-default t                         ; Nobody likes to loose work, I certainly don't
+      truncate-string-ellipsis "…")               ; Unicode ellispis are nicer than "...", and also save /precious/ space
+
+
+(display-time-mode 1)                             ; Enable time in the mode-line
+(unless (equal "Battery status not available"
+               (battery))
+  (display-battery-mode 1))                       ; On laptops it's nice to know how much power you have
+(global-subword-mode 1)                           ; Iterate through CamelCase words
+
+(setq-default custom-file (expand-file-name ".custom.el" doom-private-dir))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+(setq evil-vsplit-window-right t
+      evil-split-window-below t)
+
+(defadvice! prompt-for-buffer (&rest _)
+  :after '(evil-window-split evil-window-vsplit)
+  (+ivy/switch-buffer))
+
+(setq +ivy-buffer-preview t)
---
+++ editor/emacs/doom/packages.el
@@ -0,0 +1,30 @@
+;; -*- no-byte-compile: t; -*-
+;;; $DOOMDIR/packages.el
+
+
+(package! all-the-icons-dired)
+
+(package! evil-better-visual-line)
+
+(package! org-download
+  :recipe (:host github
+            :repo "abo-abo/org-download"))
+
+(package! org-protocol-capture-html
+  :recipe (:host github
+           :repo "alphapapa/org-protocol-capture-html"))
+
+(package! org-noter)
+
+(package! org-ref)
+
+(package! helm-org-rifle)
+
+(package! org-roam-bibtex)
+
+(package! zotxt)
+
+(package! package-lint)
+(package! flycheck-package)
+
+(package! bufler)
